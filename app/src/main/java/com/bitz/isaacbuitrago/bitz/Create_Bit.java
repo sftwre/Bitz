1195fbee9d5963a2c264c6484cade19edc39f696
Isaac Buitrago <mightyking7@gmail.com>
2018-09-14 21:15:34 -0500

    Added Media controls to bottom navigation as well as the main bit icon to the bottom navigation

--- app/src/main/java/com/bitz/isaacbuitrago/bitz/Create_Bit.java
+++ app/src/main/java/com/bitz/isaacbuitrago/bitz/Create_Bit.java
@@ -17,13 +17,13 @@ public class Create_Bit extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_skip_previous:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_skip_next:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_play:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
