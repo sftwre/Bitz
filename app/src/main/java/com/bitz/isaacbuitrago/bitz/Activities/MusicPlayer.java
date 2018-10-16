a6ddf992435321a34d8409e8031082f5da3dd205
Isaac Buitrago <mightyking7@gmail.com>
2018-10-15 23:32:12 -0500

    Added property listener for seek bar to move with the music and control the playback.

--- app/src/main/java/com/bitz/isaacbuitrago/bitz/Activities/MusicPlayer.java
+++ app/src/main/java/com/bitz/isaacbuitrago/bitz/Activities/MusicPlayer.java
@@ -1,47 +1,73 @@
 package com.bitz.isaacbuitrago.bitz.Activities;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
-
 import com.bitz.isaacbuitrago.bitz.Application.Properties;
-import com.bitz.isaacbuitrago.bitz.Model.Bit;
 import com.bitz.isaacbuitrago.bitz.R;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.PlayerApi;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.ImageUri;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
-import java.net.HttpURLConnection;
+
 import java.net.URL;
 
 /**
- * Responsible for playing a specified track and managing
- * other events involved in creating a Bit.
+ * Responsible for rendering the image and data of the currently playing
+ * track on the user's phone.
  *
  * @author isaacbuitrago
  */
 public class MusicPlayer extends AppCompatActivity
 {
-    private TextView timePlayed;
 
     private SpotifyAppRemote mSpotifyAppRemote;
 
+    private PlayerApi playerApi;
+
     private URL apiRequest;
 
-    private MusicSearcher musicSearcher;
+    private Track currentTrack;
+
+    private SeekBar seekBar;
+
+    private TextView timePlayed;
+
+    private TextView timeRemaining;
+
+    private ImageView albumCover;
+
+    private BottomNavigationView navigation;
+
+    private Handler handler;
+
+    private  Runnable runnable;
+
+    private long songDuration;
+
+    private static final long SLEEP_TIME = 1000;
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
+            switch (item.getItemId())
+            {
                 case R.id.navigation_skip_previous:
                     timePlayed.setText(R.string.title_home);
                     return true;
@@ -57,6 +83,37 @@ public class MusicPlayer extends AppCompatActivity
         }
     };
 
+    private SeekBar.OnSeekBarChangeListener mOnSeekBarChangeListener =
+
+            new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+                {
+                    if(fromUser && playerApi != null)
+                    {
+                        playerApi.seekTo((long) progress);
+                    }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar)
+                {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar)
+                {
+
+                }
+            };
+
+    /**
+     * Creates the activity, binds GUI fields to instance variables,
+     * and sets event listeners for the seek and navigation bar.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -64,22 +121,33 @@ public class MusicPlayer extends AppCompatActivity
 
         setContentView(R.layout.activity_create_bit);
 
-        timePlayed = (TextView) findViewById(R.id.message);
+        timePlayed = (TextView) findViewById(R.id.timePlayed);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        timeRemaining = (TextView) findViewById(R.id.timeRemaining);
+
+        albumCover = (ImageView) findViewById(R.id.albumCover);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(mOnSeekBarChangeListener);
+
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        musicSearcher =  new MusicSearcher();
     }
 
-
+    /**
+     * Starts the activity, connects to the Spotify process,
+     * and starts activity with the remote process.
+     *
+     */
     @Override
     protected void onStart()
     {
         super.onStart();
 
-        // Set the connection parameters
+        // Set Spotify the connection parameters
         ConnectionParams connectionParams = new ConnectionParams.Builder(Properties.CLIENT_ID)
                 .setRedirectUri(Properties.REDIRECT_URI)
                 .showAuthView(true)
@@ -88,15 +156,15 @@ public class MusicPlayer extends AppCompatActivity
         Log.i("LoginActivity", "Set connection params for spotify");
 
         // Connect to Spotify
-
         SpotifyAppRemote.CONNECTOR.connect(this, connectionParams,
                 new Connector.ConnectionListener()
                 {
-
                     @Override
                     public void onConnected(SpotifyAppRemote spotifyAppRemote)
                     {
                         mSpotifyAppRemote = spotifyAppRemote;
+
+                        playerApi = mSpotifyAppRemote.getPlayerApi();
 
                         Log.d("MainActivity", "Connected to Spotify");
 
@@ -117,8 +185,6 @@ public class MusicPlayer extends AppCompatActivity
     }
 
 
-
-
     @Override
     protected void onStop()
     {
@@ -130,32 +196,86 @@ public class MusicPlayer extends AppCompatActivity
     }
 
     /**
-     * Handler when Spotify is connected
+     * Used to interact with remote Spotify process and subscribe to it's player state.
      */
     private void spotifyConnected()
     {
-        // search for the track
-        musicSearcher.search("Harder");
-
-        // get the track image
-
-        // get the track media
-
-        mSpotifyAppRemote.getPlayerApi().play("spotify:user:spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
-
+        // play the track
+        playerApi.play("spotify:user:spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
 
         // Subscribe to PlayerState
-        mSpotifyAppRemote.getPlayerApi()
-                .subscribeToPlayerState().setEventCallback(new Subscription.EventCallback<PlayerState>() {
+        playerApi.subscribeToPlayerState().setEventCallback(new Subscription.EventCallback<PlayerState>() {
+
             @Override
-            public void onEvent(PlayerState playerState) {
+            public void onEvent(PlayerState playerState)
+            {
                 final Track track = playerState.track;
-                if (track != null) {
-                    Log.d("MainActivity", track.name + " by " + track.artist.name);
+
+                if (track != null)
+                {
+                    currentTrack = track;
+
+                    // TODO make async
+                    // set the image for the current track
+                    //renderImage(currentTrack.imageUri);
+
+                    // set upper range of the progress bar
+                    seekBar.setMax((int) currentTrack.duration);
+
+                    // change the seek bar
+                    changeSeekBar(playerState);
+
+                    // TODO make this dynamic
+                    // set the time stamps
+                    timePlayed.setText(String.format("0:00"));
+
+                    timeRemaining.setText(String.format("1:00"));
+
+                    StringBuilder builder = new StringBuilder()
+                                    .append("Playing '")
+                                    .append(track.name)
+                                    .append("' by ")
+                                    .append(track.artist.name);
+
+                    Log.i("MainActivity", builder.toString());
                 }
             }
         });
     }
 
+    /**
+     * Used to set up property listeners for the seek bar
+     *
+     * @param playerState state of the spotify player in the background
+     */
+    private void changeSeekBar(final PlayerState playerState)
+    {
+        seekBar.setProgress((int) 1000);
+
+        if(!(playerState.isPaused))
+        {
+            runnable = new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    changeSeekBar(playerState);
+                }
+            };
+
+            handler.postDelayed(runnable, SLEEP_TIME);
+        }
+    }
+
+    /**
+     * Renders an album or track image on the album cover by fetching
+     * the image from the specified URI.
+     *
+     * @param imageUri URI of the image to display on the album cover
+     */
+    private void renderImage(ImageUri imageUri)
+    {
+
+    }
 
 }
