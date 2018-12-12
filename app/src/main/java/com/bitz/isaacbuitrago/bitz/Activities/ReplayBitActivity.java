c372d6a1e08d04ac6fcda96bc1e906aa8f975a1c
Isaac Buitrago <mightyking7@gmail.com>
2018-12-12 00:25:49 -0600

    Created activity to play a received Bit

---
+++ app/src/main/java/com/bitz/isaacbuitrago/bitz/Activities/ReplayBitActivity.java
@@ -0,0 +1,26 @@
+package com.bitz.isaacbuitrago.bitz.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import com.bitz.isaacbuitrago.bitz.R;
+
+/**
+ *
+ * Replays the Bit received in the inbox
+ * @author isaacbuitrago
+ */
+public class ReplayActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_replay);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
